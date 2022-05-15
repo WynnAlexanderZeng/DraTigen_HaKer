c2d36d925a0cdb53bca425dbd8994eb67e1f37b9
ch-wynn-zeng <101097514+ch-wynn-zeng@users.noreply.github.com>
2022-05-15 19:11:18 +0800

    Create main.py

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
 import pygame
 import sys
+from random import randint
+f=open('hack_files\\smurf.txt')
+line = f.readline().strip() 
+virus=[]
+virus.append(line)
+while line:
+    line = f.readline().strip() 
+    virus.append(line)
+f.close()
 pygame.init()
+pygame.font.init()
 screen = pygame.display.set_mode(flags=pygame.NOFRAME)
 pmwidth=pygame.display.Info().current_w
 pmheight=pygame.display.Info().current_h
@@ -16,9 +26,15 @@ elif pmheight/pmwidth==0.625:
     bg=pygame.image.load('images\\bg16x10.jpg')
 else:
     bg=pygame.image.load('images\\bg_no_dpi.png')
+hackfont=pygame.font.Font('font\\Hack_Regular.ttf',15)
+hacktext=hackfont.render("",True,(0,255,0),None)
+text=''
 bg = pygame.transform.scale(bg,(pmwidth,pmheight))
 screen.blit(bg,(0,0))
 screen.blit(close,(pmwidth-60,0))
+jishu=0
+viruslen=0
+highvirus=0
 while True:
     mousex,mousey=pygame.mouse.get_pos()
     for event in pygame.event.get():
@@ -29,4 +45,22 @@ while True:
             screen.blit(closet,(pmwidth-60,0))
         else:
             screen.blit(close,(pmwidth-60,0))
+        if event.type==pygame.KEYDOWN:
+            if virus[viruslen]=='':
+                continue
+            if jishu==len(virus[viruslen]):
+                viruslen+=1
+                highvirus+=1
+                jishu=0
+                text=''
+            if jishu==len(virus[viruslen]) and viruslen==len(virus):
+                viruslen=0
+                jishu=0
+                text=''
+            high=(highvirus+1)*16
+            text+=virus[viruslen][jishu]
+            hacktext=hackfont.render(text,True,(0,255,0),None)
+            screen.blit(hacktext,(0,high))
+            jishu+=1
+            
     pygame.display.flip()
